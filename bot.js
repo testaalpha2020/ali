6574d67db351834a27f0619a8468f8bfde1afd74
TheCode78 <46994833+TheCode78@users.noreply.github.com>
2019-01-30 14:27:56 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -26,35 +26,6 @@ client.on('message', message => {
 
 
 
-const discord = require('discord.js');
-const client = new Discord.Client();
-const prefix = "البرفركس"
-
-client.on('ready', () => {
-console.log(`سجل الدخول ${client.user.tag}!`);
-console.log('')
-console.log('')
-console.log('╔[═════════════════════════════════════════════════════════════════]╗')
-console.log(`[Start] ${new Date()}`);
-console.log('╚[═════════════════════════════════════════════════════════════════]╝')
-console.log('')
-console.log('╔[════════════════════════════════════]╗');
-console.log(`تسجيل الدخول ل * [ " ${client.user.username} " ]`);
-console.log('')
-console.log('معلومات : ')
-console.log('')
-console.log(`عدد السيرفرات♤ [ " ${client.guilds.size} " ]`);
-console.log(`المستخدمين♡ [ " ${client.users.size} " ]`);
-console.log(`الرومات♧ [ " ${client.channels.size} " ]`);
-console.log('╚[════════════════════════════════════]╝')
-console.log('By Jackeo')
-console.log('╔[════════════]╗')
-console.log(' البوت اون لاين')
-console.log('╚[════════════]╝')
-console.log('V2 العربية arap ')
-console.log('me pro')
- 
-});
 
 
 
