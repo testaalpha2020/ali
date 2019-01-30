709ef6cd8a4c861151a7837b9e858375dc0a5a88
TheCode78 <46994833+TheCode78@users.noreply.github.com>
2019-01-30 13:16:19 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,9 +1,9 @@
 const Discord = require("discord.js");
 const client = new Discord.Client();
  
+ 
  client.on('message', message => {
-   if(message.content.startsWith("*invite")
-                                 invites")) {
+   if(message.content.startsWith("*invites")) {
     message.guild.fetchInvites().then(invs => {
       let user = message.mentions.users.first() || message.author
       let personalInvites = invs.filter(i => i.inviter.id === user.id);
@@ -12,7 +12,6 @@ message.channel.send(`${user} has **${inviteCount}** invites.`);
 });
   }
 });
- 
  
  
 
