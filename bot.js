b58d7eb931c167b3f9cb61099065b9639952a1bc
TheCode78 <46994833+TheCode78@users.noreply.github.com>
2019-01-30 14:59:40 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -35,6 +35,27 @@ client.on('message', message => {
 
 
 
+ 
+ client.on('message', message => {
+    if (message.content.startsWith("*link")) {
+ 
+        message.channel.createInvite({
+        thing: true,
+        maxUses: 1,
+        maxAge: 86400
+    }).then(invite =>
+      message.author.sendMessage(invite.url)
+    )
+    const embed = new Discord.RichEmbed()
+        .setColor("RANDOM")
+        .setDescription("Done  🔗|🔗  on DM")
+      message.channel.sendEmbed(embed).then(message => {message.delete(10000)})
+              const Embed11 = new Discord.RichEmbed()
+        .setColor("RANDOM")
+        .setDescription("This link will stay with you 24 hours 🔗")
+      message.author.sendEmbed(Embed11)
+    }
+});
 
 
 
