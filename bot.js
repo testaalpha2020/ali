5fdf7a8bdde69086edd820a9be85ce070825f31d
TheCode78 <46994833+TheCode78@users.noreply.github.com>
2019-01-30 14:04:50 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -26,12 +26,64 @@ client.on('message', message => {
 
 
 
+const Discord = require('discord.js');
+const client = new Discord.Client();
+const prefix = "البرفركس"
+
+client.on('ready', () => {
+console.log(`سجل الدخول ${client.user.tag}!`);
+console.log('')
+console.log('')
+console.log('╔[═════════════════════════════════════════════════════════════════]╗')
+console.log(`[Start] ${new Date()}`);
+console.log('╚[═════════════════════════════════════════════════════════════════]╝')
+console.log('')
+console.log('╔[════════════════════════════════════]╗');
+console.log(`تسجيل الدخول ل * [ " ${client.user.username} " ]`);
+console.log('')
+console.log('معلومات : ')
+console.log('')
+console.log(`عدد السيرفرات♤ [ " ${client.guilds.size} " ]`);
+console.log(`المستخدمين♡ [ " ${client.users.size} " ]`);
+console.log(`الرومات♧ [ " ${client.channels.size} " ]`);
+console.log('╚[════════════════════════════════════]╝')
+console.log('By Jackeo')
+console.log('╔[════════════]╗')
+console.log(' البوت اون لاين')
+console.log('╚[════════════]╝')
+console.log('V2 العربية arap ')
+console.log('me pro')
+ 
+});
 
 
 
 
+client.on('message', message => {
+var prefix = "*" // البريفكس
+if(message.content.startsWith(prefix +"server")){ // الامر
+  if(!message.guild.member(message.author).hasPermission("ADMINISTRATOR")) return message.reply(`**هذه الخاصية للادارة فقط** ❎ `)
+if(!message.channel.guild) return message.reply(' ');
+const millis = new Date().getTime() - message.guild.createdAt.getTime();
+const now = new Date();
+dateFormat(now, 'dddd, mmmm dS, yyyy, h:MM:ss TT');
+const verificationLevels = ['None', 'Low', 'Medium', 'Insane', 'Extreme'];
+const days = millis / 1000 / 60 / 60 / 24;
+let roles = client.guilds.get(message.guild.id).roles.map(r => r.name);
+var embed  = new Discord.RichEmbed()
+.setAuthor(message.guild.name, message.guild.iconURL)
+.addField("**🆔 Server ID:**", message.guild.id,true)
+.addField("**📅 Created On**", message.guild.createdAt.toLocaleString(),true)
+.addField("**👑 Owned by**",`${message.guild.owner.user.username}#${message.guild.owner.user.discriminator}`)
+.addField("**👥 Members**",`[${message.guild.memberCount}]`,true)
+.addField('**💬 Channels **',`**${message.guild.channels.filter(m => m.type === 'text').size}**` + ' text | Voice  '+ `**${message.guild.channels.filter(m => m.type === 'voice').size}** `,true)
+.addField("**🌍 Others **" , message.guild.region,true)
+.addField("**🔐 Roles **",`**[${message.guild.roles.size}]** Role `,true)
+.setColor('#000000')
+message.channel.sendEmbed(embed)
 
-
+}
+});
 
 
 
