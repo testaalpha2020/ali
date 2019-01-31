d1ab8e0a2859fc41265bd8703e43608ec7d8df71
TheCode78 <46994833+TheCode78@users.noreply.github.com>
2019-01-31 22:18:44 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -4,35 +4,8 @@ const client = new Discord.Client();
  
 
  
-client.on('message', message => {
 
-    if(message.content === prefix + "^unmuch") {
-                        if(!message.channel.guild) return message.reply('** This command only for servers**');
 
-if(!message.member.hasPermission('MANAGE_MESSAGES')) return message.reply(' **__ليس لديك صلاحيات__**');
-           message.channel.overwritePermissions(message.guild.id, {
-         SEND_MESSAGES: false
-
-           }).then(() => {
-               message.reply("**__تم تقفيل الشات__ :white_check_mark: **")
-           });
-             }
-//FIRE BOT
- if(message.content === prefix + "^much") {
-                     if(!message.channel.guild) return message.reply('** This command only for servers**');
-
-if(!message.member.hasPermission('MANAGE_MESSAGES')) return message.reply('**__ليس لديك صلاحيات__**');
-           message.channel.overwritePermissions(message.guild.id, {
-         SEND_MESSAGES: true
-
-           }).then(() => {
-               message.reply("**__تم فتح الشات__:white_check_mark:**")
-           });
-             }
-             
-      
-    
-});
  
  
  
