d01d01434926cb64fa63dd59d2ec795ae1b85a81
TheCode78 <46994833+TheCode78@users.noreply.github.com>
2019-01-31 22:52:49 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -814,6 +814,16 @@ client.on("message", message => {
  
 
 
+client.on('message', message => {
+   if(message.content.startsWith("^invites")) {
+    message.guild.fetchInvites().then(invs => {
+      let user = message.mentions.users.first() || message.author
+      let personalInvites = invs.filter(i => i.inviter.id === user.id);
+      let inviteCount = personalInvites.reduce((p, v) => v.uses + p, 0);
+message.channel.send(`${user} has **${inviteCount}** invites.`);
+});
+  }
+});
 
 
 
