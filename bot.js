f93c3914512813bf3db0be0e69608bb0141e7601
TheCode78 <46994833+TheCode78@users.noreply.github.com>
2019-02-01 17:00:16 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -591,7 +591,7 @@ client.on('message', message => {
         let embed = new Discord.RichEmbed()
         .setAuthor(`-#| ${message.author.username} |#-`, message.author.avatarURL)      
         .setTitle(`:small_orange_diamond:Click Here.. !`)
-        .setURL(`https://discordapp.com/oauth2/authorize?client_id=395462979115679755&scope=bot&permissions=2146958591`)
+        .setURL(`https://discordapp.com/oauth2/authorize?client_id=538678456125227019&permissions=8&scope=bot`)
         .setThumbnail(" https://cdn.discordapp.com/avatars/377904849783750667/6c76e412f18c142dfd711d05fb363869.png?size=2048")
         .addField(':small_blue_diamond:Byـ:', "<@" + message.author.id + ">")        
      message.channel.sendEmbed(embed);
