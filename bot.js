156622c1c0aaec613b856a9b704cbe11dfc209fe
TheCode78 <46994833+TheCode78@users.noreply.github.com>
2019-02-01 11:10:42 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -140,25 +140,6 @@ client.on('message', message => {
 
 
 
-client.on('message', message => {
-    if (!message.channel.guild) return;
-    if(!message.member.hasPermission('ADMINISTRATOR')) return message.reply("***  ليس معك صلاحيات  ***")
-    var prefix = "^";
-       if (message.content ===   prefix + "addrole") {
-    message.channel.send("<@" + message.author.id + ">  ***  جاري اعطاء الرتبة للاعضاء كما امرت  *** ")
-    message.guild.members.forEach(m => {
-    m.addRole(message.guild.roles.find('name', 'اسم الرتبة'))
-    })
-    }
-    
-       if (message.content ===   prefix + "remove role") {
-    message.channel.send("<@" + message.author.id + ">  ***  جاري اعطاء الرتبة للاعضاء كما امرت  *** ")
-    message.guild.members.forEach(m => {
-    m.removeRole(message.guild.roles.find('name', 'اسم الرتبة'))
-    })
-    }
-    
-    });
 
 
 
